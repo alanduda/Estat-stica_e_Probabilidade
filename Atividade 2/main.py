191d5b0b144e4b901b58687584a7d6dfb466e023
Alan Duda <alandudatop10@gmail.com>
2021-07-08 20:36:43 -0300

    end exercise 2

--- Atividade 2/main.py
+++ Atividade 2/main.py
@@ -1,7 +1,8 @@
-import random
+import matplotlib.pyplot as plt
 from math import factorial
+import random
 
-# infelizmente não consegui encontrar um dataset com variável de Bernoulli
+# infelizmente não consegui encontrar um dataset diferente do que o senhor passou no video sobre diagnostico com variável de Bernoulli
 # por isso, criei um dataset no qual ha um probabilidade de uma moeda cair cara(0) ou coroa(1)
 dataset = [[i, random.choice([0, 1])] for i in range(1000)]
 
@@ -41,4 +42,34 @@ print(f'q = {q:0.3f}')
 pq = p * q
 print(f'VAR(x) = p . q = {pq:0.3f}')
 
+#Calcular Média
+n = 5  #numero de tentativas
+average = n * p
+print(f'Media = E(x) = {average}')
+
+#Calcular Variância
+variance = n * p * q
+print(f'Variância = VAR(x) = {variance}')
+
+#Calcular desvio padrão
+standardDeviation = variance**0.5
+print(f'Desvio Padrão = {standardDeviation}')
+
 # Calcular probabilidade binomial
+odds = []
+def binomialProbability(x,n,p,q):
+  return (factorial(n)/(factorial(n-x)*factorial(x)))*(p**x)*(q**(n-x))
+
+for x in range(6):
+    px = binomialProbability(x,n,p,q)
+    print('P(',x,')=',px)
+    odds.append(px)
+
+print('Apartir dos resultados abaixo é possivel perceber que a maior probabilidade do jogar tirar uma sequenciade de acerto seria 3 jogadas')
+x = [0,1,2,3,4,5]
+plt.bar(x,odds)
+plt.xlabel('x')
+plt.ylabel('P(x)')
+plt.title('Distribuição de Probabilidade')
+plt.grid(True)
+plt.show()
